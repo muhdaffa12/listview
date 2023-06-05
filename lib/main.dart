01f9b1f449a7b0658c89cb8ed983ae7b92fb3314
muhdaffa12 <mdr307545@gmail.com>
2023-06-06 02:23:00 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List View"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.add_business_outlined),
+            title: Text("Sensor Daffa 1"),
+          )
+        ],
+      ),
+    );
+  }
+}
